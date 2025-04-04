b1a2c338e3dc9711fa03d29a3f08f5350d69bd8a
Ananya Pentapati <22APentapati@heckgrammar.co.uk>
2025-04-04 10:04:19 +0100

    rememeber jeevs birthday is november something

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace BlankCSDotNet8
+﻿using System.Text;
+
+namespace BlankCSDotNet8
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            DateTime current = DateTime.Now;
+            DateTime target = new DateTime(2025, 4, 4, 15, 15, 0);
+            TimeSpan difference = target - current;
+
+            Console.WriteLine(current);
+            Console.WriteLine(target);
+            Console.WriteLine(difference);      // 7th of spetember is humnas birthday 
+            Thread.Sleep(1000);                 // 22nd of december is pareens birthday 
+
+
         }
     }
 }
