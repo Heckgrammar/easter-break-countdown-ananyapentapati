e861558b5161b33987a1cf0b52f21ea27dbdbcfd
Ananya Pentapati <22APentapati@heckgrammar.co.uk>
2025-04-04 10:15:19 +0100

    gdytgevf

--- BlankCSDotNet8/Program.cs
+++ BlankCSDotNet8/Program.cs
@@ -6,16 +6,16 @@ namespace BlankCSDotNet8
     {
         static void Main(string[] args)
         {
-            DateTime current = DateTime.Now;
-            DateTime target = new DateTime(2025, 4, 4, 15, 15, 0);
-            TimeSpan difference = target - current;
+            
 
-            Console.WriteLine(current);
-            Console.WriteLine(target);
-            Console.WriteLine(difference);      // 7th of spetember is humnas birthday 
-            Thread.Sleep(1000);                 // 22nd of december is pareens birthday 
-
-
+            while (true) 
+            {
+                DateTime current = DateTime.Now;
+                DateTime target = new DateTime(2025, 4, 4, 15, 15, 0);
+                TimeSpan difference = target - current;
+                Console.WriteLine(difference);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
